d94bf84cfc11ecf64786d9396f78e9e73d486606
yusifizcode <5ba0gh2@code.edu.az>
2022-03-15 02:10:45 +0400

    csharp introduction tasks

---
+++ csharpIntro/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace csharpIntro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Verilmis array-deki reqemlerinin cemi 10-dan kicik olan ededlerin sayini tapan alqoritm
+            //int[] arr = { 13, 29, 32, 43, 89 };
+            //int sum = 0, count=0, digit=0;
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    while (0 < arr[i])
+            //    {
+            //        digit = arr[i]%10;
+            //        sum = sum + digit;
+            //        arr[i] = arr[i] / 10;
+
+            //    }
+            //    if (sum > 10)
+            //        {
+            //            count++;
+            //        }
+            //    sum = 0;
+
+            //}
+            //Console.WriteLine("Count --> "+count);
+
+
+            //2. 1-den 8000-dek ededler icerisinde cut olanlarin sayini tapan proqram
+            //int count = 0;
+
+            //for (int i = 1; i < 8000; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine(count);
+
+
+            //3.Verilmis ededin en yaxin asagi kokalti deyerini tapan alqoritm(ilk ders etdiyimize uygun, misalcun eded 20 - dirse 4 qaytarmalidi, 25 - dise 5 qaytarmalidi, 16 - drisa 4 ve s.)
+            //int number = 35;
+            //int newNumber=0;
+
+            //for (int i = 1; i*i <= number; i++)
+            //{
+            //    newNumber = i;
+            //}
+            //Console.WriteLine(newNumber);
+
+        }
+    }
+}
